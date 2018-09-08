e849c1b27aaa0bff50f4fb8e3c55302e099c2d3a
Ted <tedwuusa@gmail.com>
2018-09-09 03:31:53 +0800

    Add smart contract calls from client

--- chain-estate/dashboard/main.js
+++ chain-estate/dashboard/main.js
@@ -3,9 +3,12 @@ import { default as contract } from 'truffle-contract';
 
 window.ChainEstateApp = {
     buildingContract: null,
+    spaceContract: null,
     buildingAddress: null,
+    spaceAddress: null,
     buildingOwner: null,
     account: null,
+    spaces: null,
 
     InitPage: function() {
         self = this;
@@ -40,9 +43,12 @@ window.ChainEstateApp = {
         // Load contract
         var p = [];
         p.push(this.GetContract('../contracts/Building.json', c => {self.buildingContract=c}));
+        p.push(this.GetContract('../contracts/Space.json', c => {self.spaceContract=c}));
+        var buildingInst;
         Promise.all(p).then(function() {
             return self.buildingContract.deployed();
         }).then(function(inst) {
+            buildingInst = inst;
             self.buildingAddress = inst.address;
             console.log("Building Contract At: " + inst.address);
 
@@ -51,6 +57,13 @@ window.ChainEstateApp = {
         }).then(function(value) {
             self.buildingOwner = value.valueOf();
             console.log("Building Owner: " + self.buildingOwner);
+
+            return buildingInst.spaces.call();
+        }).then(function(spInst) {
+            console.log("Space Contract At: " + spInst.valueOf());
+
+            self.spaceContract.at(spInst.valueOf());
+            self.spaceAddress = spInst.valueOf();
 
             self.ReloadData();
         });
@@ -78,10 +91,12 @@ window.ChainEstateApp = {
                 self.account = null;
                 self.CheckRedirect(2);
             }
+
+            self.GetAllSpaces();
         }).catch(function(error) {
             console.error(error);
-            self.account = null;
-            self.CheckRedirect(2);
+            //self.account = null;
+            //self.CheckRedirect(2);
         });
     },
 
@@ -114,6 +129,86 @@ window.ChainEstateApp = {
             }).catch(function(err){
                 reject(err);
             });
+        });
+    },
+
+    GetSpace: function(inst, owner, index, result) {
+        return new Promise(function(resolve, reject) {
+            console.log("Getting Space " + index);
+
+            var prom, id;
+            if (owner)
+                prom = inst.tokenOfOwnerByIndex.call(owner, index);
+            else
+                prom = inst.tokenByIndex.call(index);
+
+            prom.then(function(val) {
+                id = val.valueOf();
+
+                return inst.getSpaceInfo.call(id);
+            }).then(function(res) {
+                var v = res.valueOf();
+                result.id = id;
+                result.name = v[0];
+                result.floor = v[1].toNumber();
+                result.size = v[2].toNumber();
+                result.price = v[3].toNumber();
+                result.status = v[4].toNumber();
+                result.leasee = v[5];
+                result.enddate = v[6];
+
+                resolve();
+            }).catch(function(err) {
+                reject(err);
+            })
+        });
+    },
+
+    GetAllSpaces: function() {
+        self = this;
+
+        var buildingInst;
+
+        console.log("Getting all spaces");
+        self.spaceContract.at(self.spaceAddress).totalSupply.call().then(function(value) {
+            let count = value.valueOf();
+            console.log("Total Spaces: " + count);
+
+            self.spaces = [];
+            
+            var p = [];
+            for (let i = 0; i < count; i++) {
+                var r = {};
+                self.spaces.push(r);
+                p.push(self.GetSpace(self.spaceContract.at(self.spaceAddress), null, i, r));    
+            }
+
+            return Promise.all(p);
+        }).then(function() {
+            console.log(self.spaces);
+        }).catch(function(err) {
+            console.error(err);
+        });
+    },
+
+    StartLease: function(id, enddate) {
+        console.log("Starting lease: " + id);
+
+        self.spaceContract.at(self.spaceAddress).startLease(id, "2019-09-10").then(function() {
+            GetAllSpaces();
+        }).catch(function(err) {
+            console.error(err);
+        });
+    },
+
+    SplitSpace: function(id, size, price) {
+        console.log("Splitting space: " + id);
+
+        self.spaceContract.at(self.spaceAddress).splitSpace(id, size, price).then(function(newid) {
+            console.log("New space created: " + newid.valueOf());
+            GetAllSpaces();
+        }).catch(function(err) {
+            console.error(err);
         });
     }
 };
